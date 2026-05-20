f6f5dc5a42a9a33121866de32b3889315fb61551
kyomoaaa <kayriz.pearla@ui.ac.id>
2026-05-20 13:41:32 +0700

    baru dashboard doang

---
+++ finpro1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void tampilanDashboard() {
+    printf("Welcome Caretaker! :3\n");
+    printf("Silahkan isi pertanyaan dibawah ini dengan skala (0-5)\n");
+    printf("0: Tidak Sama Sekali\n1: Sangat Rendah\n2: Rendah\n3: Sedang\n4: Tinggi\n5: Sangat Tinggi\n");
+}
+
+int main(void) {
+    tampilanDashboard();
+    return 0;
+}
